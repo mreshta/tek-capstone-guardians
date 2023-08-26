b2aef049ee95b3d28d4b3f4950d380f8cb432089
mreshta <mreshta10@gmail.com>
2023-08-26 01:26:48 -0700

    the

--- src/test/java/tek/capstone/guardians/runner/TestRunner.java
+++ src/test/java/tek/capstone/guardians/runner/TestRunner.java
@@ -8,7 +8,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		
-		tags = "@createNewAccount",
+		tags = "@login",
 		features = ("classpath:features"),
 		glue = ("tek.capstone.guardians"),
 		dryRun = false,
