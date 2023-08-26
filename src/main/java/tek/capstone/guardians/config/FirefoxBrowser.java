46ee658edff24e8d26d5a200d9d6778c6b0a0e2e
mreshta <mreshta10@gmail.com>
2023-08-26 14:53:50 -0700

    t

--- src/main/java/tek/capstone/guardians/config/FirefoxBrowser.java
+++ src/main/java/tek/capstone/guardians/config/FirefoxBrowser.java
@@ -2,17 +2,20 @@ package tek.capstone.guardians.config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FirefoxBrowser implements Browser{
 
 
-	public WebDriver openBrowser(String url) {
-		WebDriverManager.firefoxdriver().setup();
-		WebDriver driver = new FirefoxDriver();
-		driver.get(url);
-		return driver;
-	}
-
+	  public WebDriver openBrowser(String url) {
+	    	WebDriverManager.firefoxdriver().setup();
+	        FirefoxOptions options = new FirefoxOptions();
+	        options.setHeadless(true);
+	        WebDriver driver = new FirefoxDriver(options);
+	        driver.get(url);
+	        return driver;
+	    }
 	
 }
